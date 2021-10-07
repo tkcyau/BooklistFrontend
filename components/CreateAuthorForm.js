134168d8a3746be90ad0b30068eb15a0d3dc23c5
Tim <tkcyau@gmail.com>
2021-10-07 06:45:13 -0400

    Add create book on Author create

--- components/CreateAuthorForm.js
+++ components/CreateAuthorForm.js
@@ -4,6 +4,7 @@ import Router from 'next/router';
 import styled from 'styled-components';
 import useForm from '../lib/useForm';
 import { ALL_AUTHORS_QUERY } from './AuthorsList';
+import BookForm from './CreateBookForm';
 
 const CreateFormWrapper = styled.div`
   margin-bottom: 24px;
@@ -19,10 +20,25 @@ const CreateFormWrapper = styled.div`
     border-bottom: 1px solid lightgrey;
   }
 `;
+const AuthorFieldsGroup = styled.div`
+  margin-bottom: 16px;
+`;
+const BookFieldsGroup = styled.div``;
 
 const CREATE_AUTHOR_MUTATION = gql`
-  mutation CREATE_AUTHOR_MUTATION($authorName: String!, $dateOfBirth: String!) {
-    createAuthor(data: { name: $authorName, dateOfBirth: $dateOfBirth }) {
+  mutation CREATE_AUTHOR_MUTATION(
+    $authorName: String!
+    $dateOfBirth: String!
+    $title: String
+    $year: Int
+  ) {
+    createAuthor(
+      data: {
+        name: $authorName
+        dateOfBirth: $dateOfBirth
+        book: { create: { title: $title, year: $year } }
+      }
+    ) {
       id
     }
   }
@@ -32,6 +48,8 @@ export default function CreateAuthorForm() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
     authorName: '',
     dateOfBirth: '',
+    title: '',
+    year: '',
   });
 
   const [createAuthor, { loading, error, data }] = useMutation(
@@ -40,6 +58,8 @@ export default function CreateAuthorForm() {
       variables: {
         authorName: inputs.authorName,
         dateOfBirth: inputs.dateOfBirth,
+        title: inputs.title,
+        year: inputs.year,
       },
       refetchQueries: [{ query: ALL_AUTHORS_QUERY }],
     }
@@ -59,30 +79,56 @@ export default function CreateAuthorForm() {
         }}
       >
         <fieldset disabled={loading}>
-          <label htmlFor="authorName">
-            Author Name
-            <input
-              required
-              type="text"
-              id="authorName"
-              name="authorName"
-              placeholder="Author Name"
-              value={inputs.authorName}
-              onChange={handleChange}
-            />
-          </label>
-          <label htmlFor="dateOfBirth">
-            Author Date of Birth
-            <input
-              type="date"
-              id="dateOfBirth"
-              name="dateOfBirth"
-              placeholder="Author Date of Birth"
-              value={inputs.dateOfBirth}
-              onChange={handleChange}
-            />
-          </label>
-          <button type="submit">Add Author</button>
+          <AuthorFieldsGroup>
+            <label htmlFor="authorName">
+              Author Name
+              <input
+                required
+                type="text"
+                id="authorName"
+                name="authorName"
+                placeholder="Author Name"
+                value={inputs.authorName}
+                onChange={handleChange}
+              />
+            </label>
+            <label htmlFor="dateOfBirth">
+              Author Date of Birth
+              <input
+                type="date"
+                id="dateOfBirth"
+                name="dateOfBirth"
+                placeholder="Author Date of Birth"
+                value={inputs.dateOfBirth}
+                onChange={handleChange}
+              />
+            </label>
+            <button type="submit">Add Author</button>
+          </AuthorFieldsGroup>
+          <BookFieldsGroup>
+            <label htmlFor="title">
+              Book title
+              <input
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Book title"
+                value={inputs.title}
+                onChange={handleChange}
+              />
+            </label>
+            <label htmlFor="year">
+              Year
+              <input
+                type="number"
+                id="year"
+                name="year"
+                placeholder="Year"
+                value={inputs.year}
+                onChange={handleChange}
+              />
+            </label>
+          </BookFieldsGroup>
         </fieldset>
       </form>
     </CreateFormWrapper>
